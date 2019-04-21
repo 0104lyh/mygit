8ab14e4d41159000ff69d49170229161ee1221cf
0104lyh <48237468+0104lyh@users.noreply.github.com>
2019-04-22 00:12:16 +0800

    Create 先序输出叶结点.cpp

---
+++ 先序输出叶结点.cpp
@@ -0,0 +1,56 @@
+/*本题要求按照先序遍历的顺序输出给定二叉树的叶结点。
+
+函数接口定义：
+void PreorderPrintLeaves( BinTree BT );
+其中BinTree结构定义如下：
+
+typedef struct TNode *Position;
+typedef Position BinTree;
+struct TNode{
+    ElementType Data;
+    BinTree Left;
+    BinTree Right;
+};
+函数PreorderPrintLeaves应按照先序遍历的顺序输出给定二叉树BT的叶结点，格式为一个空格跟着一个字符。
+
+裁判测试程序样例：
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char ElementType;
+typedef struct TNode *Position;
+typedef Position BinTree;
+struct TNode{
+    ElementType Data;
+    BinTree Left;
+    BinTree Right;
+};
+
+BinTree CreatBinTree(); /* 实现细节忽略 */
+void PreorderPrintLeaves( BinTree BT );
+
+int main()
+{
+    BinTree BT = CreatBinTree();
+    printf("Leaf nodes are:");
+    PreorderPrintLeaves(BT);
+    printf("\n");
+
+    return 0;
+}
+*/
+void PreorderPrintLeaves(BinTree BT)
+{
+	if (BT)
+	{
+		if (!BT->Right&&!BT->Left)
+		{
+			printf(" %c", BT->Data);
+		}
+		else
+		{
+			PreorderPrintLeaves(BT->Left);
+			PreorderPrintLeaves(BT->Right);
+		}
+	}
+}
